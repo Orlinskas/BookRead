ad1da85de451289142b0d6301fa00d9f544df0aa
orlinskas <orlinskaslove@gmail.com>
2019-07-07 23:10:21 +0300

    заполнена активность помощь

--- app/src/main/java/com/orlinskas/bookread/activities/MainActivity.java
+++ app/src/main/java/com/orlinskas/bookread/activities/MainActivity.java
@@ -1,10 +1,11 @@
 package com.orlinskas.bookread.activities;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
@@ -22,7 +23,6 @@ import com.orlinskas.bookread.AppContext;
 import com.orlinskas.bookread.R;
 import com.orlinskas.bookread.ToastBuilder;
 import com.orlinskas.bookread.constants.PermissionConstant;
-import com.orlinskas.bookread.data.SharedPreferencesData;
 import com.orlinskas.bookread.helpers.ActivityOpenHelper;
 
 public class MainActivity extends AppCompatActivity
@@ -30,33 +30,38 @@ public class MainActivity extends AppCompatActivity
 
     int permissionStatus;
     TextView test;
-    //Context context = getApplicationContext();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         test = findViewById(R.id.tv_test);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                permissionStatus = ContextCompat.checkSelfPermission
+                        (getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+                if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
+                    ActivityOpenHelper.openActivity(getApplicationContext(), FileManagerActivity.class);
+                }
+                else {
+                    ToastBuilder.create(getApplicationContext(), "Вы не разрешили приложению доступ к памяти телефона!");
+                }
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         //parse();
@@ -72,7 +77,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -105,7 +110,7 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -127,13 +132,18 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_help) {
             ActivityOpenHelper.openActivity(getApplicationContext(), HelpActivity.class);
         } else if (id == R.id.nav_share) {
-
+            Intent sendIntent = new Intent();
+            sendIntent.setType("text/plain");
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "\"BookRead\" - английский в твоих книгах!");
+            sendIntent.putExtra(Intent.EXTRA_TEXT,  "\"BookRead\" - английский в твоих книгах! Присоединяйся https://play.google.com/store?hl=ru");
+            startActivity(Intent.createChooser(sendIntent,"Поделиться"));
         } else if (id == R.id.group) {
             ActivityOpenHelper.openActivity(getApplicationContext(), AboutAsActivity.class);
         } else if (id == R.id.nav_send_author) {
             ActivityOpenHelper.openActivity(getApplicationContext(), ContactActivity.class);
         }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
