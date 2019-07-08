7aa3bd8a8332c5e88232ab2a3223cafb5da9675f
alex <alzholus@gmail.com>
2019-07-08 19:16:07 +0200

    BookBodyFileReader class refactor

--- app/src/main/java/com/orlinskas/bookread/helpers/BookBodyFileReader.java
+++ app/src/main/java/com/orlinskas/bookread/helpers/BookBodyFileReader.java
@@ -4,106 +4,45 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class BookBodyFileReader {
+    private Character[] characters = new Character[]{
+            ' ', '\n', '.', ',', '!', '&', '?', ':', ';',
+            '@', '#', '$', '%', '*', '(', ')', '-'/*минус*/, '‐'/*дефиз*/,
+            '–'/*короткое тире*/, '—'/*длинное тире*/, '+', '«', '»'
+    };
 
     private ArrayList<String> words = new ArrayList<>();
     private StringBuilder word = new StringBuilder();
 
-    public ArrayList<String> read (File file) throws Exception {
-        BufferedReader bufferedReader;
-        bufferedReader = new BufferedReader(new FileReader(file));
+    public ArrayList<String> read(File file) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
 
         int c;
+
         while ((c = bufferedReader.read()) != -1) {
-            switch ((char) c){
-                case ' ':
-                    separateWord(" ");
-                    break;
-                case '\n':
-                    separateWord("\n");
-                    break;
-                case '.':
-                    separateWord(".");
-                    break;
-                case ',':
-                    separateWord(",");
-                    break;
-                case '!':
-                    separateWord("!");
-                    break;
-                case '&':
-                    separateWord("&");
-                    break;
-                case '?':
-                    separateWord("?");
-                    break;
-                case ':':
-                    separateWord(":");
-                    break;
-                case ';':
-                    separateWord(";");
-                    break;
-                case '@':
-                    separateWord("@");
-                    break;
-                case '#':
-                    separateWord("#");
-                    break;
-                case '$':
-                    separateWord("$");
-                    break;
-                case '%':
-                    separateWord("%");
-                    break;
-                case '*':
-                    separateWord("*");
-                    break;
-                case '(':
-                    separateWord("(");
-                    break;
-                case ')':
-                    separateWord(")");
-                    break;
-                case '-': //минус
-                    separateWord("-");
-                    break;
-                case '‐': //дефиз
-                    separateWord("‐");
-                    break;
-                case '–': //короткое тире
-                    separateWord("–");
-                    break;
-                case '—': //длинное тире
-                    separateWord("—");
-                    break;
-                case '+':
-                    separateWord("+");
-                    break;
-                case '«':
-                    separateWord("«");
-                    break;
-                case '»':
-                    separateWord("»");
-                    break;
-                    default:
-                        word.append((char) c);
+            char charValue = (char) c;
+            String stringValue = String.valueOf(charValue);
+
+            if (Arrays.asList(characters).contains(charValue)) {
+                separateWord(stringValue);
+            } else {
+                word.append(charValue);
             }
         }
+
         bufferedReader.close();
 
         return words;
     }
 
     private void separateWord(String character) {
-        if(word.length() > 0) {
+        if (word.length() > 0) {
             words.add(word.toString());
-            words.add(character);
         }
-        else {
-            words.add(character);
-        }
+
+        words.add(character);
         this.word = new StringBuilder();
     }
-
 }
